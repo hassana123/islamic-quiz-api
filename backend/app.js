dfe6a1653b9acd93807d0791287fe8c05ae71900
hassana123 <hassanaabdll1@gmail.com>
2024-09-05 04:26:29 -0700

    updates

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,9 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    'http://localhost:5173',  
+    'http://localhost:5173', 
+    'http://localhost:8080',
+    'https://islamic-questions-api.vercel.app/', 
     'https://hallaly-islamic-questions-api.vercel.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
